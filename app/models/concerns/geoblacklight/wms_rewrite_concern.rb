1a8f7f416dd25e202a8d5ae545eb8dce7e543cc7
dl-maura <maura_carbone@harvard.edu>
2023-03-01 09:22:03 -0500

    Create wms_rewrite_concern.rb

---
+++ app/models/concerns/geoblacklight/wms_rewrite_concern.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module WmsRewriteConcern
+  extend Geoblacklight::SolrDocument
+
+  def viewer_endpoint
+    if local_restricted?
+      # replace wms prefix with cas authed proxy
+      super.gsub(Settings.INSTITUTION_GEOSERVER_URL, Settings.PROXY_GEOSERVER_URL)
+    else
+      super
+    end
+  end
+
+  def local_restricted?
+    local? && restricted?
+  end
+
+  def local?
+    fetch(Settings.FIELDS.PROVENANCE, "").casecmp(Settings.INSTITUTION_LOCAL_NAME).zero?
+  end
+end
